3b8120dcdd21bf9e95ae90eb63c46ea4c776e30f
Maxwell Benton <maxwellbenton@gmail.com>
2024-07-07 19:35:14 -0400

    feat(project): local dev setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Mushroom } from "./components/Mushroom";
+import { Gnome } from "./components/Gnome";
+import { Chest } from "./components/Chest";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Mushroom />
+      <Mushroom />
+      <Mushroom />
+      <Gnome key={1} gnome={1} position={{x: 100, y: 100}}/>
+      <Gnome key={2} gnome={2} position={{x: 135, y: 100, facing: 'left'}}/>
+      <Chest />
+    </div>
+  );
 }
 
-export default App
+export default App;
