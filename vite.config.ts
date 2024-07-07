b67cb30155cd8945c6183ee98d390e214d5bb6a1
Maxwell Benton <maxwellbenton@gmail.com>
2024-07-07 19:34:19 -0400

    feat(project): app configuration and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.warn("process.env", process.env);
+  return defineConfig({
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [react()],
+    build: {
+      lib: {
+        entry: "./src/index.tsx",
+        name: "gnome-components",
+        fileName: (format) => `gnome-components.${format}.js`,
+      },
+      target: "esnext",
+    },
+  });
+};
